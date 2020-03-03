59b45d30709b2727b342ff932449ace7c16c18f5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-03 12:07:18 +0000

    Deploy Digitopia/nouvellecuisine to github.com/Digitopia/nouvellecuisine.git:gh-pages

---
+++ precache-manifest.bb01e8cc991908bc2de32c0998af4041.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69db9149f46165d3f90f",
+    "url": "/nouvellecuisine/css/app.132261b1.css"
+  },
+  {
+    "revision": "3d32703dc8817438e829b6b92c4dc348",
+    "url": "/nouvellecuisine/img/bg.jpg"
+  },
+  {
+    "revision": "1c634e795aac3b29a9a54245495b1f50",
+    "url": "/nouvellecuisine/img/logo.png"
+  },
+  {
+    "revision": "b0bafdaf6b1c2c46e3ad596b677ee6ba",
+    "url": "/nouvellecuisine/img/wall.jpg"
+  },
+  {
+    "revision": "06e6336358e2711b359fdfb354b89f62",
+    "url": "/nouvellecuisine/index.html"
+  },
+  {
+    "revision": "69db9149f46165d3f90f",
+    "url": "/nouvellecuisine/js/app.012f0da6.js"
+  },
+  {
+    "revision": "4929040e9a75d2286625",
+    "url": "/nouvellecuisine/js/chunk-vendors.3f42ad51.js"
+  },
+  {
+    "revision": "be517af2f48d02a49eb2673f36460bfa",
+    "url": "/nouvellecuisine/manifest.json"
+  },
+  {
+    "revision": "b20de8bd6b90068147c681b14b73262f",
+    "url": "/nouvellecuisine/pratos/gelado-limao.html"
+  },
+  {
+    "revision": "f3844e007dc118907c4776ef725a0fe2",
+    "url": "/nouvellecuisine/pratos/pasteis-bacalhau.html"
+  },
+  {
+    "revision": "03255bb9ccf8687e561f2dc374b3ebea",
+    "url": "/nouvellecuisine/pratos/rodizio.html"
+  },
+  {
+    "revision": "a79391fd613ba2fd36c9d85c49cc2455",
+    "url": "/nouvellecuisine/pratos/sushi.html"
+  },
+  {
+    "revision": "2e2808f30f2d976ac74a2d6dc2d49cf0",
+    "url": "/nouvellecuisine/pratos/vichyssoise.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nouvellecuisine/robots.txt"
+  }
+]);
